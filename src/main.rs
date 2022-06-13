17594dba90182764a7a2b45f0cdb9021063dd18d
Erik Funder Carstensen <fundererik@fastmail.com>
2022-06-13 11:56:39 +0200

    Update dependencies and add AMC

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,70 @@
 #![feature(never_type)]
-use lambda_runtime::{self, handler_fn, Context, Error};
+use lambda_runtime::{self, service_fn, Error, LambdaEvent};
+use phf::phf_map;
 use serde_json::{json, Value};
+
+const RC: &str = "https://www.facebook.com/groups/regnecentralen.au";
+const ACADEMY: &str = "https://game.academy.beer";
+const AMC: &str = "https://brightspace.au.dk/d2l/home/54990";
+
+static COURSES: phf::Map<&'static str, &'static str> = phf_map! {
+    "amc" => AMC,
+    "hovedkontor" => RC, "regnecentralen" => RC, "rc" => RC,
+    "d" => ACADEMY, "dispensation" => ACADEMY,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    lambda_runtime::run(handler_fn(func)).await
+    let func = service_fn(func);
+    lambda_runtime::run(func).await
 }
 
-async fn func(value: Value, _: Context) -> Result<impl serde::Serialize, !> {
-    if let Some(path) = value
-        .get("pathParameters")
-        .map(|pps| pps.get("urlParam"))
-        .flatten()
-        .map(|p| {
-            if let Value::String(s) = p {
-                Some(s.as_str())
-            } else {
-                None
-            }
-        })
-        .flatten()
-    {
-        match path {
-            "test" | "oversigt" => {
+async fn func(event: LambdaEvent<Value>) -> Result<Value, !> {
+    let (event, _cxt) = event.into_parts();
+    let path = event["pathParameters"]
+        .as_object()
+        .and_then(|m| m.get("urlParam"))
+        .and_then(Value::as_str)
+        .unwrap_or("");
+    match path {
+        "test" | "oversigt" => Ok(json!({
+            "statusCode": u16::from(http::StatusCode::OK),
+            "headers": {
+                "content-type": "text/html"
+            },
+            "body": include_str!("oversigt.html"),
+            "isBase64Encoded": false
+        })),
+        s if s.starts_with("toast") => {
+            if rand::random::<u8>() <= 25 {
                 return Ok(json!({
                     "statusCode": u16::from(http::StatusCode::OK),
                     "headers": {
                         "content-type": "text/html"
                     },
-                    "body": include_str!("oversigt.html"),
+                    "body": include_str!("bamboozeld.html"),
                     "isBase64Encoded": false
                 }));
             }
-            "dispensation" | "d" => {
-                return Ok(json!({
-                    "statusCode": u16::from(http::StatusCode::FOUND),
-                    "headers": {
-                        "location": "https://game.academy.beer/login"
-                    },
-                    "body": "Redirecting...",
-                    "isBase64Encoded": false
-                }))
-            }
-            s if s.starts_with("toast") => {
-                if rand::random::<u8>() <= 25 {
-                    return Ok(json!({
-                        "statusCode": u16::from(http::StatusCode::OK),
-                        "headers": {
-                            "content-type": "text/html"
-                        },
-                        "body": include_str!("bamboozeld.html"),
-                        "isBase64Encoded": false
-                    }));
-                }
-                return Ok(json!({
-                    "statusCode": u16::from(http::StatusCode::FOUND),
-                    "headers": {
-                        "location": "https://bamboozle.academy"
-                    },
-                    "body": "Redirecting...",
-                    "isBase64Encoded": false
-                }));
-            }
-            "rc" | "regnecentralen" | "hovedkontor" => {
-                return Ok(json!({
-                    "statusCode": u16::from(http::StatusCode::FOUND),
-                    "headers": {
-                        "location": "https://www.facebook.com/groups/regnecentralen.au"
-                    },
-                    "body": "Redirecting...",
-                    "isBase64Encoded": false
-                }))
-            }
-            _ => (),
+            Ok(json!({
+                "statusCode": u16::from(http::StatusCode::FOUND),
+                "headers": {
+                    "location": "https://bamboozle.academy"
+                },
+                "body": "Redirecting...",
+                "isBase64Encoded": false
+            }))
+        }
+        rest => {
+            let &url = COURSES.get(rest).unwrap_or(&"https://brightspace.au.dk");
+            Ok(json!({
+                "statusCode": u16::from(http::StatusCode::FOUND),
+                "headers": {
+                    "location": url
+                },
+                "body": "Redirecting...",
+                "isBase64Encoded": false
+            }))
         }
     }
-    Ok(json!({
-        "statusCode": u16::from(http::StatusCode::FOUND),
-        "headers": {
-            "location": "https://brightspace.au.dk"
-        },
-        "body": "Redirecting...",
-        "isBase64Encoded": false
-    }))
 }
